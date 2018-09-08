67c196017d35cfbc15bda89d40c8e5e653d2c324
mitulpatel224 <mitul.patel224@gmail.com>
2018-09-08 16:31:45 +0530

    added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularDraggableModule } from 'angular2-draggable';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsModule, TooltipModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StyleGeneratorComponent } from './style-generator/style-generator.component';
-import { TabsModule } from 'ngx-bootstrap';
 import { CopyToClipboardDirective } from './core/copy-to-clipboard.directive';
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Routes = [
+  {
+    path: 'app', component: StyleGeneratorComponent,
+    data: {
+      animation: {
+        value: '3',
+      }
+    }
+  },
+  {
+    path: 'home', component: HomeComponent,
+    data: {
+      animation: {
+        value: '1',
+      }
+    }
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     StyleGeneratorComponent,
     CopyToClipboardDirective,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(routes),
     AngularDraggableModule,
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
